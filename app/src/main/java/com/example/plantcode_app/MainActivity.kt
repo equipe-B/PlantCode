5a98174e255da06607843ec38636afad21ec628a
DougCruzz10 <douglascruztst11@gmail.com>
2024-11-24 11:15:20 -0300

    Feat: Adm data model created

--- app/src/main/java/com/example/plantcode_app/MainActivity.kt
+++ app/src/main/java/com/example/plantcode_app/MainActivity.kt
@@ -1,36 +1,13 @@
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.seuapp.data.model.Administrador
-import com.seuapp.data.model.Permissoes
-import com.seuapp.data.repository.AdministradorRepository
+import com.seuapp.modelo.adicionarAdministrador // Importe a função
 
 class MainActivity : AppCompatActivity() {
-
-    private val administradorRepository = AdministradorRepository()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val permissoes = Permissoes(gerenciarUsuarios = true, acessoAnalitico = true)
-        val novoAdministrador = Administrador(
-            nome = "Carlos Pereira",
-            email = "carlos.pereira@example.com",
-            dataCriacao = "2024-01-01",
-            permissoes = permissoes
-        )
-
-        adicionarAdministrador("admin3", novoAdministrador)
-    }
-
-    private fun adicionarAdministrador(id: String, administrador: Administrador) {
-        administradorRepository.adicionarAdministrador(id, administrador,
-            onSuccess = {
-                println("Administrador adicionado com sucesso!")
-            },
-            onFailure = { e ->
-                println("Erro ao adicionar administrador: ${e.message}")
-            }
-        )
+        // Chame a função para adicionar um novo administrador para teste
+        adicionarAdministrador("Admin Teste", "admin.teste@example.com", "senhaSegura", "Admin")
     }
 }
--- app/src/main/java/com/example/plantcode_app/Pacotes/Administradores/Administradores.kt
+++ app/src/main/java/com/example/plantcode_app/Pacotes/Administradores/Administradores.kt
@@ -1,8 +1,30 @@
 
+import android.util.Log
+import com.google.firebase.firestore.FirebaseFirestore
 
 data class Administrador(
-    val nome: String,
-    val email: String,
-    val dataCriacao: String,
-    val permissoes: Permissoes
+    val nome: String =  "",
+    val email: String = "",
+    val senha: String = ""
 )
+
+
+
+// Função para adicionar um novo administrador
+fun adicionarAdministrador(nome: String, email: String, senha: String, nivelAcesso: String) {
+    val db = FirebaseFirestore.getInstance()
+    val novoAdministrador = Administrador(
+        nome = nome,
+        email = email,
+        senha = senha,
+    )
+
+    db.collection("Administradores")
+        .add(novoAdministrador)
+        .addOnSuccessListener { documentReference ->
+            Log.d("Firestore", "Administrador adicionado com ID: ${documentReference.id}")
+        }
+        .addOnFailureListener { e ->
+            Log.w("Firestore", "Erro ao adicionar administrador", e)
+        }
+}
